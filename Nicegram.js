a6456ce76e8ef7a3b0dc327a9a03a1999e64da73
Aniya <105577952+leqwq@users.noreply.github.com>
2022-12-06 15:24:15 +0800

    Update Nicegram.js

--- Nicegram.js
+++ Nicegram.js
@@ -2,14 +2,13 @@
 
 脚本功能：Nicegram: Unlimited Text+解锁订阅
 下载地址：https://is.gd/7OPpId
-使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️
 
 *******************************
 
 [rewrite_local]
 
-https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Nicegram.js
-
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/leqwq/QuantumultX/main/Nicegramleqwq.js
 [mitm] 
 hostname=restore-access.indream.app
 
